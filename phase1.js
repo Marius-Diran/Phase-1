63e37d4523e8e42e82555eabc97b18799bc2999c
Marius Odediran <diranmarius8@gmail.com>
2024-02-15 23:01:54 +0100

    Js Training

---
+++ phase1.js
@@ -0,0 +1,16 @@
+// console.log("I like pizza");
+
+let Name = "Marius";
+let age = 20;
+let stats = true;
+// let username = window.prompt('What is your name?');
+
+let header1 = document.querySelector('.h1');
+let header2 = document.querySelector('.h2');
+let header3 = document.querySelector('.h3');
+
+header1.textContent = "Hello " + Name;
+header2.textContent = "I am " + age + " Years Old."
+header3.textContent = "Enrolled: " + stats;
+
+// console.log(username);
